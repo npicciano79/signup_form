bbd6ee0a5d41ba954eb21a3dcb9730db34a869a7
npicciano79 <npicciano79@gmail.com>
2022-07-22 09:01:17 -0400

    javascript

---
+++ javascript/javascript_login.js
@@ -0,0 +1,43 @@
+const newMemberSelect=document.querySelector('#new_member_signup');
+const loginButton=document.querySelector('#member_submit');
+const username=document.querySelector('#member_username');
+const loginPage=document.querySelector('login');
+const password=document.querySelector('#member_password');
+const forgotPassSelect=document.querySelector("#forgot_password");
+
+
+
+
+//function hides login display and displays options to retrieve password
+function forgotPassFunction(){
+    
+
+
+}
+
+newMemberSelect.addEventListener('click',()=>{
+    hideLoginScreen()
+})
+
+
+
+
+loginButton.addEventListener('click',()=>{
+    alert('hi');
+});
+
+
+forgotPassSelect.addEventListener('click',()=>{
+
+    //loginPage.style.display='none';
+    alert('hit');
+    //forgotPassFunction();
+    
+});
+
+
+function hideLoginScreen(){
+    loginPage.
+
+}
+
